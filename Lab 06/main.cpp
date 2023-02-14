5694f7c6854a76a24850bc95055d309d495f26f2
grimoire <hf0218@ENGVLAB084>
2023-02-13 19:15:03 -0500

    started main.cpp

--- Lab 06/main.cpp
+++ Lab 06/main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
 #include "point2d.h"
+#include "point2d.cpp"
+#include "rectangletype.h"
+#include "rectangletype.cpp"
 using namespace std;
 
+int main() {
+
+	int ax0;
+	int ay0;
+	int ax1;
+	int ay1;
+
+	cout << "Rectangle App!" << endl;
+	cout << "--------------" << endl;
+
+	cout << "Please enter an input for x0: ";
+	cin >> ax0;
+	cout << "Please enter an input for y0: ";
+	cin >> ay0;
+	cout << "Please enter an input for x1: ";
+	cin >> ax1;
+	cout << "Please enter an input for y1: ";
+	cin >> ay1;
+
+
+
+
+
+
+}
--- Lab 06/rectangletype.h
+++ Lab 06/rectangletype.h
@@ -8,32 +8,32 @@ private:
     point2D p1;
 
 public:
-    int getX0() {
+    int getX0(){
         return p0.getX();
     }
-    int getY0() {
+    int getY0(){
         return p0.getY();
     }
-    int getX1() {
+    int getX1(){
         return p1.getX();
     }
-    int getY1() {
+    int getY1(){
         return p1.getY();
     }
-    int getWidth() {
-        return (getX1() - getX0());
-    }
-    int getLength() {
+    int getWidth(){
         return (getY1() - getY0());
     }
-    int getPerimeter() {
-        return 2*(getWidth() + getLength());
+    int getLength(){
+        return (getX1() - getX0());
     }
-    int getArea() {
-        return (getWidth() * getLength());
+    int getPerimeter(){
+        return 2*(getWidth()+getLength());
+    }
+    int getArea(){
+        int Area = (getWidth()*getLength());
+        return Area;
     }
     rectangleType(int ax0, int ay0, int ax1, int ay1) {
-        point2D p0(ax0, ay0);
-        point2D p1(ax1, ay1);
+
     }
 };
